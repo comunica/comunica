comunica/comunica

commit 4537d8c38667140dcec006494e2c5b03f4049c44

Use AsyncReiterable for holding datasources instead of an array
This allows sources to be passed as a stream,
and the number of sources to be (potentially) endless and appendable.

This is still backwards-compatible with array input.

packages/actor-rdf-resolve-quad-pattern-federated/lib/FederatedQuadSource.ts
@@ -1,6 +1,6 @@
 import {
-  IActionRdfResolveQuadPattern, IActorRdfResolveQuadPatternOutput,
-  ILazyQuadSource, KEY_CONTEXT_SOURCE, KEY_CONTEXT_SOURCES,
+  DataSources, IActionRdfResolveQuadPattern,
+  IActorRdfResolveQuadPatternOutput, IDataSource, ILazyQuadSource, KEY_CONTEXT_SOURCE, KEY_CONTEXT_SOURCES,
 } from "@comunica/bus-rdf-resolve-quad-pattern";
 import {ActionContext, Actor, IActorTest, Mediator} from "@comunica/core";
 import {AsyncIterator, EmptyIterator} from "asynciterator";
@@ -8,7 +8,6 @@ import {PromiseProxyIterator} from "asynciterator-promiseproxy";
 import {RoundRobinUnionIterator} from "asynciterator-union";
 import {blankNode, quad} from "rdf-data-model";
 import * as RDF from "rdf-js";
-import {IQuerySource} from "./ActorRdfResolveQuadPatternFederated";
 
 /**
  * A FederatedQuadSource can evaluate quad pattern queries over the union of different heterogeneous sources.
@@ -18,7 +17,7 @@ export class FederatedQuadSource implements ILazyQuadSource {
 
   protected readonly mediatorResolveQuadPattern: Mediator<Actor<IActionRdfResolveQuadPattern, IActorTest,
     IActorRdfResolveQuadPatternOutput>, IActionRdfResolveQuadPattern, IActorTest, IActorRdfResolveQuadPatternOutput>;
-  protected readonly sources: IQuerySource[];
+  protected readonly sources: DataSources;
   protected readonly contextDefault: ActionContext;
   protected readonly emptyPatterns: {[sourceHash: string]: RDF.Quad[]};
   protected readonly skipEmptyPatterns: boolean;
@@ -35,12 +34,12 @@ export class FederatedQuadSource implements ILazyQuadSource {
 
     // Initialize sources in the emptyPatterns datastructure
     if (this.skipEmptyPatterns) {
-      for (const source of this.sources) {
+      this.sources.iterator().on('data', (source: IDataSource) => {
         const sourceHash: string = FederatedQuadSource.hashSource(source);
         if (!this.emptyPatterns[sourceHash]) {
           this.emptyPatterns[sourceHash] = [];
         }
-      }
+      });
     }
   }
 
@@ -49,7 +48,7 @@ export class FederatedQuadSource implements ILazyQuadSource {
    * @param {IQuerySource} source A query source object.
    * @return {string} A string representation of this query source.
    */
-  public static hashSource(source: IQuerySource): string {
+  public static hashSource(source: IDataSource): string {
     return JSON.stringify(source);
   }
 
@@ -89,7 +88,7 @@ export class FederatedQuadSource implements ILazyQuadSource {
    * @param {RDF.Quad} pattern
    * @return {boolean}
    */
-  public isSourceEmpty(source: IQuerySource, pattern: RDF.Quad) {
+  public isSourceEmpty(source: IDataSource, pattern: RDF.Quad) {
     if (!this.skipEmptyPatterns) {
       return false;
     }
@@ -117,9 +116,12 @@ export class FederatedQuadSource implements ILazyQuadSource {
 
     // Counters for our metadata
     const metadata: {[id: string]: any} = { totalItems: 0 };
-    let remainingSources: number = this.sources.length;
+    let remainingSources: number = 0;
 
-    const it: RoundRobinUnionIterator<RDF.Quad> = new RoundRobinUnionIterator(this.sources.map((source) => {
+    const sourcesIt = this.sources.iterator();
+    const it: RoundRobinUnionIterator<RDF.Quad> = new RoundRobinUnionIterator(sourcesIt.map((source) => {
+      remainingSources++;
+
       // If we can predict that the given source will have no bindings for the given pattern,
       // return an empty iterator.
       const pattern: RDF.Quad = quad(subject || blankNode(), predicate || blankNode(), object || blankNode(),
@@ -177,9 +179,11 @@ export class FederatedQuadSource implements ILazyQuadSource {
     });
 
     // If we have 0 sources, immediately emit metadata
-    if (!remainingSources) {
-      setImmediate(() => it.emit('metadata', metadata));
-    }
+    sourcesIt.on('end', () => {
+      if (!remainingSources) {
+        it.emit('metadata', metadata);
+      }
+    });
 
     return it;
   }

packages/actor-context-preprocess-rdf-source-identifier/lib/ActorContextPreprocessRdfSourceIdentifier.ts
@@ -1,8 +1,9 @@
 import {ActorContextPreprocess,
   IActorContextPreprocessOutput} from "@comunica/bus-context-preprocess";
+import {DataSources, IDataSource, KEY_CONTEXT_SOURCES} from "@comunica/bus-rdf-resolve-quad-pattern";
 import {IActionRdfSourceIdentifier, IActorRdfSourceIdentifierOutput} from "@comunica/bus-rdf-source-identifier";
 import {ActionContext, Actor, IAction, IActorArgs, IActorTest, Mediator} from "@comunica/core";
-import {KEY_CONTEXT_SOURCES} from "../../bus-rdf-resolve-quad-pattern";
+import {AsyncReiterableArray} from "asyncreiterable";
 
 /**
  * A comunica RDF Source Identifier Context Preprocess Actor.
@@ -22,22 +23,40 @@ export class ActorContextPreprocessRdfSourceIdentifier extends ActorContextPrepr
 
   public async run(action: IAction): Promise<IActorContextPreprocessOutput> {
     if (action.context && action.context.get(KEY_CONTEXT_SOURCES)) {
-      const sources = action.context.get(KEY_CONTEXT_SOURCES);
-      const autoSources = sources.map((source: any, id: number) => ({ id, source }))
-        .filter((entry: any) => entry.source.type === 'auto');
       const subContext: ActionContext = action.context.delete(KEY_CONTEXT_SOURCES);
-      const autoSourceTypePromises: Promise<IActorRdfSourceIdentifierOutput>[] = autoSources.map(
-        (entry: any) => this.mediatorRdfSourceIdentifier.mediate(
-          { sourceValue: entry.source.value, context: subContext }));
-      const autoSourceTypes: IActorRdfSourceIdentifierOutput[] = await Promise.all(autoSourceTypePromises);
-      for (let i = 0; i < autoSources.length; i++) {
-        const sourceId = autoSources[i].id;
-        const sourceType = autoSourceTypes[i].sourceType;
-        if (sourceType) {
-          sources[sourceId].type = sourceType;
+
+      const sources: DataSources = action.context.get(KEY_CONTEXT_SOURCES);
+      const newSources: DataSources = AsyncReiterableArray.forInitialEmpty();
+      let remainingSources = 1;
+      const it = sources.iterator();
+      it.on('data', (source: IDataSource) => {
+        remainingSources++;
+        if (source.type === 'auto') {
+          this.mediatorRdfSourceIdentifier.mediate(
+            { sourceValue: source.value, context: subContext })
+            .then((sourceIdentificationResult) => {
+              if (sourceIdentificationResult.sourceType) {
+                source.type = sourceIdentificationResult.sourceType;
+              }
+              newSources.push(source);
+              endSource();
+            });
+        } else {
+          newSources.push(source);
+          endSource();
+        }
+      });
+      it.on('end', () => {
+        endSource();
+      });
+
+      function endSource() {
+        if (--remainingSources === 0) {
+          newSources.push(null);
         }
       }
-      return { context: action.context.set(KEY_CONTEXT_SOURCES, sources) };
+
+      return { context: action.context.set(KEY_CONTEXT_SOURCES, newSources) };
     }
     return action;
   }

packages/actor-context-preprocess-rdf-source-identifier/test/ActorContextPreprocessRdfSourceIdentifier-test.ts
@@ -1,6 +1,8 @@
 import {ActorContextPreprocess} from "@comunica/bus-context-preprocess";
 import {ActionContext, Bus} from "@comunica/core";
+import {AsyncReiterableArray} from "asyncreiterable";
 import {ActorContextPreprocessRdfSourceIdentifier} from "../lib/ActorContextPreprocessRdfSourceIdentifier";
+const arrayifyStream = require('arrayify-stream');
 
 describe('ActorContextPreprocessRdfSourceIdentifier', () => {
   let bus;
@@ -49,41 +51,55 @@ describe('ActorContextPreprocessRdfSourceIdentifier', () => {
       return expect(actor.run({ context: ActionContext({}) })).resolves.toMatchObject({ context: {} });
     });
 
-    it('should run for a context with zero sources', () => {
-      return expect(actor.run({ context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-            [] }) })).resolves
-        .toMatchObject({ context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [] }) });
+    it('should run for a context with zero sources', async () => {
+      return expect(await arrayifyStream((await actor.run({
+        context: ActionContext(
+          { '@comunica/bus-rdf-resolve-quad-pattern:sources': AsyncReiterableArray.forFixedData([]) }),
+      })).context.get('@comunica/bus-rdf-resolve-quad-pattern:sources').iterator()))
+        .toEqual([]);
     });
 
-    it('should run for a context with two dummy sources', () => {
-      return expect(actor.run({ context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-            [{ type: 'dummy' }, { type: 'dummy' }] }) })).resolves
-        .toMatchObject({ context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-              [{ type: 'dummy' }, { type: 'dummy' }] }) });
+    it('should run for a context with two dummy sources', async () => {
+      return expect(await arrayifyStream((await actor.run({
+        context: ActionContext(
+          { '@comunica/bus-rdf-resolve-quad-pattern:sources': AsyncReiterableArray.forFixedData([
+              { type: 'dummy' },
+              { type: 'dummy' },
+          ]) }),
+      })).context.get('@comunica/bus-rdf-resolve-quad-pattern:sources').iterator()))
+        .toEqual([{ type: 'dummy' }, { type: 'dummy' }]);
     });
 
-    it('should run for a context with two auto sources', () => {
-      const context = { context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-            [{ type: 'auto', value: 'abc' }, { type: 'auto', value: 'def' }] }) };
-      return expect(actor.run(context)).resolves
-        .toMatchObject({ context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-              [{ type: 'a', value: 'abc' }, { type: 'd', value: 'def' }] }) });
+    it('should run for a context with two auto sources', async () => {
+      return expect(await arrayifyStream((await actor.run({
+        context: ActionContext(
+          { '@comunica/bus-rdf-resolve-quad-pattern:sources': AsyncReiterableArray.forFixedData([
+              { type: 'auto', value: 'abc' },
+              { type: 'auto', value: 'def' },
+          ]) }),
+      })).context.get('@comunica/bus-rdf-resolve-quad-pattern:sources').iterator()))
+    .toEqual([{ type: 'a', value: 'abc' }, { type: 'd', value: 'def' }]);
     });
 
-    it('should run for a context with a auto and a dummy source', () => {
-      const context = { context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-            [{ type: 'auto', value: 'abc' }, { type: 'dummy' }] }) };
-      return expect(actor.run(context)).resolves
-        .toMatchObject({ context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-              [{ type: 'a', value: 'abc' }, { type: 'dummy' }] }) });
+    it('should run for a context with a auto and a dummy source', async () => {
+      return expect(await arrayifyStream((await actor.run({
+        context: ActionContext(
+          { '@comunica/bus-rdf-resolve-quad-pattern:sources': AsyncReiterableArray.forFixedData([
+              { type: 'auto', value: 'abc' },
+              { type: 'dummy' },
+          ]) }),
+      })).context.get('@comunica/bus-rdf-resolve-quad-pattern:sources').iterator()))
+    .toEqual([{ type: 'dummy' }, { type: 'a', value: 'abc' }]);
     });
 
-    it('should run and keep the auto type if the mediator fails', () => {
-      const context = { context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-            [{ type: 'auto' }] }) };
-      return expect(actor.run(context)).resolves
-        .toMatchObject({ context: ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-              [{ type: 'auto' }] }) });
+    it('should run and keep the auto type if the mediator fails', async () => {
+      return expect(await arrayifyStream((await actor.run({
+        context: ActionContext(
+          { '@comunica/bus-rdf-resolve-quad-pattern:sources': AsyncReiterableArray.forFixedData([
+              { type: 'auto' },
+          ]) }),
+      })).context.get('@comunica/bus-rdf-resolve-quad-pattern:sources').iterator()))
+    .toEqual([{ type: 'auto' }]);
     });
   });
 });

packages/actor-init-sparql/lib/ActorInitSparql-browser.ts
@@ -8,6 +8,7 @@ import {IActionRootSparqlParse, IActorOutputRootSparqlParse,
   IActorTestRootSparqlParse} from "@comunica/bus-sparql-serialize";
 import {IActorSparqlSerializeOutput} from "@comunica/bus-sparql-serialize";
 import {ActionContext, Actor, IAction, IActorArgs, IActorTest, Mediator} from "@comunica/core";
+import {AsyncReiterableArray} from "asyncreiterable";
 import * as RDF from "rdf-js";
 import {termToString} from "rdf-string";
 import {QUAD_TERM_NAMES} from "rdf-terms";
@@ -106,6 +107,9 @@ export class ActorInitSparql extends ActorInit implements IActorInitSparqlArgs {
     if (context.queryFormat) {
       context[KEY_CONTEXT_QUERYFORMAT] = context.queryFormat;
       delete context.queryFormat;
+    }
+    if (Array.isArray(context[KEY_CONTEXT_SOURCES])) {
+      context[KEY_CONTEXT_SOURCES] = AsyncReiterableArray.forFixedData(context[KEY_CONTEXT_SOURCES]);
     }
 
     context = ActionContext(context);

packages/actor-rdf-resolve-quad-pattern-federated/lib/ActorRdfResolveQuadPatternFederated.ts
@@ -55,8 +55,3 @@ export interface IActorRdfResolveQuadPatternFederatedArgs
     IActorRdfResolveQuadPatternOutput>, IActionRdfResolveQuadPattern, IActorTest, IActorRdfResolveQuadPatternOutput>;
   skipEmptyPatterns?: boolean;
 }
-
-export interface IQuerySource {
-  type: string;
-  value: any;
-}

packages/actor-rdf-resolve-quad-pattern-federated/test/ActorRdfResolveQuadPatternFederated-test.ts
@@ -1,6 +1,7 @@
 import {ActorRdfResolveQuadPattern} from "@comunica/bus-rdf-resolve-quad-pattern";
 import {ActionContext, Bus} from "@comunica/core";
 import {ArrayIterator} from "asynciterator";
+import {AsyncReiterableArray} from "asyncreiterable";
 import {ActorRdfResolveQuadPatternFederated} from "../lib/ActorRdfResolveQuadPatternFederated";
 const squad = require('rdf-quad');
 const arrayifyStream = require('arrayify-stream');
@@ -78,10 +79,11 @@ describe('ActorRdfResolveQuadPatternFederated', () => {
 
     it('should run', () => {
       const pattern = squad('?s', 'p', 'o', '?g');
-      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-      ]});
+      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+          ])});
       return actor.run({ pattern, context })
         .then(async (output) => {
           expect(await output.metadata()).toEqual({ totalItems: 4 });
@@ -99,19 +101,21 @@ describe('ActorRdfResolveQuadPatternFederated', () => {
       const thisActor = new ActorRdfResolveQuadPatternFederated(
         { name: 'actor', bus, mediatorResolveQuadPattern: thisMediator, skipEmptyPatterns });
       const pattern = squad('?s', 'p', 'o', '?g');
-      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-      ]});
+      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+          ])});
       return expect(thisActor.run({ pattern, context })).resolves.toBeTruthy();
     });
 
     it('should run when only metadata is called', () => {
       const pattern = squad('?s', 'p', 'o', '?g');
-      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-      ]});
+      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+          ])});
       return expect(actor.run({ pattern, context })
         .then((output) => output.metadata()))
         .resolves.toEqual({ totalItems: 4 });
@@ -119,10 +123,11 @@ describe('ActorRdfResolveQuadPatternFederated', () => {
 
     it('should run when only data is called', () => {
       const pattern = squad('?s', 'p', 'o', '?g');
-      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-      ]});
+      const context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+          ])});
       return actor.run({ pattern, context })
         .then(async (output) => {
           expect(await arrayifyStream(output.data)).toEqual([

packages/actor-rdf-resolve-quad-pattern-federated/test/FederatedQuadSource-test.ts
@@ -1,6 +1,7 @@
 import {ActionContext} from "@comunica/core";
 import {ArrayIterator, EmptyIterator} from "asynciterator";
 import {RoundRobinUnionIterator} from "asynciterator-union";
+import {AsyncReiterableArray} from "asyncreiterable";
 import {blankNode, defaultGraph, literal, namedNode, quad, variable} from "rdf-data-model";
 import {FederatedQuadSource} from "../lib/FederatedQuadSource";
 const squad = require('rdf-quad');
@@ -37,7 +38,7 @@ describe('FederatedQuadSource', () => {
       },
     };
     context = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-        [ { type: 'myType', value: 'myValue' } ] });
+        AsyncReiterableArray.forFixedData([ { type: 'myType', value: 'myValue' } ]) });
   });
 
   describe('The FederatedQuadSource module', () => {
@@ -308,7 +309,8 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [] });
+      contextEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([]) });
       source = new FederatedQuadSource(mediator, contextEmpty, emptyPatterns, true);
     });
 
@@ -333,7 +335,7 @@ describe('FederatedQuadSource', () => {
     beforeEach(() => {
       emptyPatterns = {};
       contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
-          [ { type: 'emptySource', value: 'I will be empty' } ] });
+          AsyncReiterableArray.forFixedData([ { type: 'emptySource', value: 'I will be empty' } ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 
@@ -373,9 +375,8 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingle = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-      ] });
+      contextSingle = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([{ type: 'nonEmptySource', value: 'I will not be empty' }]) });
       source = new FederatedQuadSource(mediator, contextSingle, emptyPatterns, true);
     });
 
@@ -415,10 +416,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'emptySource', value: 'I will be empty' },
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'emptySource', value: 'I will be empty' },
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 
@@ -463,10 +465,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'emptySource', value: 'I will be empty' },
-        { type: 'emptySource', value: 'I will be empty' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'emptySource', value: 'I will be empty' },
+            { type: 'emptySource', value: 'I will be empty' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 
@@ -507,10 +510,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'emptySource', value: 'I will be empty' },
-        { type: 'emptySource', value: 'I will be empty' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'emptySource', value: 'I will be empty' },
+            { type: 'emptySource', value: 'I will be empty' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, false);
     });
 
@@ -546,10 +550,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-        { type: 'nonEmptySource2', value: 'I will also not be empty' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+            { type: 'nonEmptySource2', value: 'I will also not be empty' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 
@@ -593,10 +598,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-        { type: 'nonEmptySourceNoMeta', value: 'I will also not be empty, but have no metadata' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+            { type: 'nonEmptySourceNoMeta', value: 'I will also not be empty, but have no metadata' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 
@@ -626,10 +632,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySourceNoMeta', value: 'I will not be empty' },
-        { type: 'nonEmptySourceNoMeta', value: 'I will also not be empty, but have no metadata' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySourceNoMeta', value: 'I will not be empty' },
+            { type: 'nonEmptySourceNoMeta', value: 'I will also not be empty, but have no metadata' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 
@@ -659,10 +666,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySource', value: 'I will not be empty' },
-        { type: 'nonEmptySourceInfMeta', value: 'I will also not be empty, but have inf metadata' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySource', value: 'I will not be empty' },
+            { type: 'nonEmptySourceInfMeta', value: 'I will also not be empty, but have inf metadata' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 
@@ -692,10 +700,11 @@ describe('FederatedQuadSource', () => {
 
     beforeEach(() => {
       emptyPatterns = {};
-      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources': [
-        { type: 'nonEmptySourceInfMeta', value: 'I will not be empty' },
-        { type: 'nonEmptySourceInfMeta', value: 'I will also not be empty, but have inf metadata' },
-      ] });
+      contextSingleEmpty = ActionContext({ '@comunica/bus-rdf-resolve-quad-pattern:sources':
+          AsyncReiterableArray.forFixedData([
+            { type: 'nonEmptySourceInfMeta', value: 'I will not be empty' },
+            { type: 'nonEmptySourceInfMeta', value: 'I will also not be empty, but have inf metadata' },
+          ]) });
       source = new FederatedQuadSource(mediator, contextSingleEmpty, emptyPatterns, true);
     });
 

packages/bus-rdf-resolve-quad-pattern/lib/ActorRdfResolveQuadPattern.ts
@@ -1,5 +1,6 @@
 import {ActionContext, Actor, IAction, IActorArgs, IActorOutput, IActorTest} from "@comunica/core";
 import {AsyncIterator} from "asynciterator";
+import {AsyncReiterable} from "asyncreiterable";
 import * as RDF from "rdf-js";
 
 /**
@@ -65,7 +66,7 @@ export interface IDataSource {
   type: string;
   value: any;
 }
-export type DataSources = IDataSource[];
+export type DataSources = AsyncReiterable<IDataSource>;
 /**
  * @type {string} Context entry for data sources.
  * @value {DataSources} An array or stream of sources.

commit 893161fc51cf69520411420b87650af0375c8a2b

Fix federation actor occasionally failing to produce results
This was a regression since 4537d8c38667140dcec006494e2c5b03f4049c44

Closes #282

packages/actor-rdf-resolve-quad-pattern-federated/lib/FederatedQuadSource.ts
@@ -116,7 +116,7 @@ export class FederatedQuadSource implements ILazyQuadSource {
 
     // Counters for our metadata
     const metadata: {[id: string]: any} = { totalItems: 0 };
-    let remainingSources: number = 0;
+    let remainingSources: number = 1;
 
     const sourcesIt = this.sources.iterator();
     const it: RoundRobinUnionIterator<RDF.Quad> = new RoundRobinUnionIterator(sourcesIt.map((source) => {
@@ -180,7 +180,7 @@ export class FederatedQuadSource implements ILazyQuadSource {
 
     // If we have 0 sources, immediately emit metadata
     sourcesIt.on('end', () => {
-      if (!remainingSources) {
+      if (!--remainingSources) {
         it.emit('metadata', metadata);
       }
     });