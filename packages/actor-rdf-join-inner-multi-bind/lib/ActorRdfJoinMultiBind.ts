comunica/comunica

commit c6ed42aa75c9be9622fd11a60d2d923f1f092c18

Disable autoStart on bind-join streams
This fixes issues with long-running queries after end during link
traversal.

packages/actor-rdf-join-inner-multi-bind/lib/ActorRdfJoinMultiBind.ts
@@ -60,7 +60,7 @@ export class ActorRdfJoinMultiBind extends ActorRdfJoin {
         .map(operation => materializeOperation(operation, bindings, { bindFilter: false }));
       const bindingsMerger = (subBindings: Bindings): Bindings | undefined => subBindings.merge(bindings);
       return new TransformIterator(async() => (await operationBinder(subOperations, bindings))
-        .transform({ map: bindingsMerger }), { maxBufferSize: 128 });
+        .transform({ map: bindingsMerger }), { maxBufferSize: 128, autoStart: false });
     };
 
     // Create an iterator that binds elements from the base stream in different orders

commit ae893cb62dc47c5fab4ea334526fa0a28af10581

Fix performance regression of TPF queries
Regression since c6ed42aa75c9be9622fd11a60d2d923f1f092c18

packages/actor-rdf-join-inner-multi-bind/lib/ActorRdfJoinMultiBind.ts
@@ -55,6 +55,9 @@ export class ActorRdfJoinMultiBind extends ActorRdfJoin {
     optional: boolean,
     bindingsFactory: BindingsFactory,
   ): BindingsStream {
+    // Enable auto-start on sub-bindings during depth-first binding for best performance.
+    const autoStartSubBindings = bindOrder === 'depth-first';
+
     // Create bindings function
     const binder = (bindings: Bindings): BindingsStream => {
       // We don't bind the filter because filters are always handled last,
@@ -63,7 +66,7 @@ export class ActorRdfJoinMultiBind extends ActorRdfJoin {
         .map(operation => materializeOperation(operation, bindings, bindingsFactory, { bindFilter: true }));
       const bindingsMerger = (subBindings: Bindings): Bindings | undefined => subBindings.merge(bindings);
       return new TransformIterator(async() => (await operationBinder(subOperations, bindings))
-        .transform({ map: bindingsMerger }), { maxBufferSize: 128, autoStart: false });
+        .transform({ map: bindingsMerger }), { maxBufferSize: 128, autoStart: autoStartSubBindings });
     };
 
     // Create an iterator that binds elements from the base stream in different orders